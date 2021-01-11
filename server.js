14ec0999bd1964d4cdbdcb46ccfea01da91e8832
Chris <34993144+Mesiow@users.noreply.github.com>
2021-01-11 11:17:45 -0500

    add new blog route

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-
 app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+/*Home Page*/
+app.get("/blogs", (req, res) => {
     res.render("index");
+});
+
+//Post Route
+app.get("/new", (req, res) => {
+    res.render("blogs/new");
 });
 
 const PORT = 5000;
