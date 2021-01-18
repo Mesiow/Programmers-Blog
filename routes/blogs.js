738d4f516242c4916a08595099386e10e3f59b52
Chris <34993144+Mesiow@users.noreply.github.com>
2021-01-18 12:02:20 -0500

    connect mongo database/post and get working

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
-
 /*node.js middleware for handling uploading files*/
 const multer = require("multer");
+
+/*Models*/
+const Blog = require("../models/Blog");
 
 //Where to upload the file
 const storage = multer.diskStorage({
@@ -32,13 +34,12 @@ const upload = multer({storage: storage, fileFilter: fileFilter});
 
 //Get
 /*Home Page*/
-router.get("/", (req, res) => {
-    const blogs = [{
-        title: "Top 10 C++ Projects Ideas for C++ Beginners",
-        desc: "Test",
-        date: new Date()
-    }];
-    res.render("index", {blogs: blogs});
+router.get("/", async (req, res) => {
+    //Get blogs from Database
+    await Blog.find({}, (err, blogs) => {
+        if(err) console.log(err);
+        res.render("index", {blogs: blogs});
+    });
 });
 
 //New blog Route
@@ -56,8 +57,14 @@ router.post("/", upload.any(), (req, res, next) => {
             desc: req.body.desc,
             filename: req.files[0].filename,
             markdown: req.body.markdown,
+            date: Date.now()
         }
-        //Upload to database
+
+        //Save to database
+        const blog = new Blog(newBlog);
+        blog.save((err) => {
+            if(err) console.log(err);
+        });
         res.redirect("/");
     }else{
         console.log("No File uploaded");
