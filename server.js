fe17f6c30a39f1c4b3bbe79beb28e737c2c9904b
Chris <34993144+Mesiow@users.noreply.github.com>
2021-01-22 10:52:40 -0500

    mongoose depracation fix/ignore file uploads

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
+mongoose.set('useCreateIndex', true);
 mongoose.connect("mongodb://localhost/blog", {
     useNewUrlParser: true,
     useUnifiedTopology: true
