f8c59f4fd4ca54dbbb1260f7d60b3b508167db15
Chris <34993144+Mesiow@users.noreply.github.com>
2021-01-22 10:47:09 -0500

    add slugify and marked packages

--- models/Blog.js
+++ models/Blog.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify"); //used for adding name of blog in the url instead of its' id
+const marked = require("marked"); //used for markdown
 
 const blogSchema = new mongoose.Schema({
-    /*
     author:{
         id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
         username: String 
-    },*/
+    },
     title: {
         type: String,
         required: true
@@ -27,7 +28,22 @@ const blogSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    } 
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+//Pre validate before saving article to db
+blogSchema.pre('validate', function(next) {
+    if(this.title){
+        console.log("validate");
+        this.slug = slugify(this.title, {lower: true, 
+        strict: true});
+    }
+    next();
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
