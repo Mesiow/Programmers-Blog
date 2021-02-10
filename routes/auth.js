4ce508c244b72cbdddc305fc37805bb427accf7b
Chris <34993144+Mesiow@users.noreply.github.com>
2021-02-10 11:40:34 -0500

    simple login and register routes implemented with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+
+const User = require("../models/User");
 
 
 //Auth Routes
@@ -9,6 +12,39 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
     res.render("auth/register");
+});
+
+router.post("/login", async(req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if(result){
+                console.log("login success");
+                res.redirect("/");
+            }else{
+                console.log("login failed");
+            }
+        });
+    }catch{
+        res.redirect("/register");
+    }
+});
+
+
+router.post("/register", async(req, res) => {
+    try{
+        const hashedPass = await bcrypt.hash(req.body.password, 10);
+        const user = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass
+        });
+        await user.save();
+
+        res.redirect("/login");
+    }catch{
+        res.redirect("/register");
+    }
 });
 
 
